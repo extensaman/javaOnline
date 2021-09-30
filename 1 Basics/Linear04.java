fb9911d4590601ae1bb81ec053c74bdb0c354456
extensaman <49030354+extensaman@users.noreply.github.com>
2021-09-30 15:00:15 +0300

    Раздел "Линейные программы". Задача №4.
    Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях). 
    Поменять местами дробную и целую части числа и вывести полученное значение числа.

---
+++ 1 Basics/Linear04.java
@@ -0,0 +1,20 @@
+﻿package by.home.basics;
+
+/*
+ * Раздел "Линейные программы".
+ * Задача №4.
+ * Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях). 
+ * Поменять местами дробную и целую части числа и вывести полученное значение числа.
+ */
+
+public class Linear04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double r = 999.888;
+		int temp = (int) r;
+		double result = r*1000.0 - temp*1000.0 + temp/1000.0;
+		System.out.println("\"R\"=" + r + '\n' + "The result is " + result);
+	}
+
+}
