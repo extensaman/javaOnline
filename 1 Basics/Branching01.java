dec8cf2c4c84194026be56036270e6265b82d5ad
extensaman <49030354+extensaman@users.noreply.github.com>
2021-09-30 16:52:47 +0300

    Раздел "Ветвления". Задача №1.
    Даны два угла треугольника (в градусах). Определить, существует ли такой треугольник, и если да, то будет ли он прямоугольным.

---
+++ 1 Basics/Branching01.java
@@ -0,0 +1,30 @@
+﻿package by.home.basics;
+
+/*
+ * Раздел "Ветвления".
+ * Задача №1.
+ * Даны два угла треугольника (в градусах). Определить, существует ли такой треугольник, и если да, то будет ли он прямоугольным.
+ */
+
+public class Branching01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int angle01 = 40;
+		int angle02 = 50;
+		if (angle01 + angle02 < 180) {
+			System.out.print("Triangle with angle #1 = " +
+									angle01 + " degrees and angle #2 = " +
+									angle02 + " degrees EXISTS");
+			if (angle01 == 90 || angle02 == 90 || 180 - angle01 - angle02 == 90) {
+				System.out.println(" and it's rectangular");
+			}
+		}
+		else {
+			System.out.print("Triangle with angle #1 = " +
+					angle01 + " degrees and angle #2 = " +
+					angle02 + " degrees doesn't EXIST");
+		}
+	}
+
+}
