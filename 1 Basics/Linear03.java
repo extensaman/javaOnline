b445aa4b057b0f44a8bf66952e662af21e6bc205
extensaman <49030354+extensaman@users.noreply.github.com>
2021-09-30 14:35:58 +0300

    Раздел "Линейные программы". Задача №3.
    Вычислить значение выражения по формуле (все переменные принимают действительные значения):
     (sin x + cos y) / (cos x - sin y) * tg xy

---
+++ 1 Basics/Linear03.java
@@ -0,0 +1,20 @@
+﻿package by.home.basics;
+
+/*
+ * Раздел "Линейные программы".
+ * Задача №3.
+ * Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+ * (sin x + cos y) / (cos x - sin y) * tg xy
+ */
+
+public class Linear03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x,y,result;
+		x = 2.0;
+		y = 10.0;
+		result = (Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y)) * Math.tan(x*y);
+		System.out.println("\"x\"=" + x + '\t' + "\"y\"=" + y + '\n' + "The result of \"(sin x + cos y) / (cos x - sin y) * tg xy\" is " + result);
+	}
+}
