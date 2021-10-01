b43a4bfc4608522f31a42d35cf9626b5b36cfb6c
extensaman <49030354+extensaman@users.noreply.github.com>
2021-10-01 15:27:46 +0300

    Раздел "Ветвления". Задача №2.
    Найти max{min(a, b), min(c, d)}.

---
+++ 1 Basics/Branching02.java
@@ -0,0 +1,35 @@
+﻿package by.home.basics;
+
+/*
+ * Раздел "Ветвления".
+ * Задача №2.
+ * Найти max{min(a, b), min(c, d)}.
+ */
+
+public class Branching02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 50;
+		int b = 200;
+		int c = 30;
+		int d = 40;
+		int minAB, minCD;
+		
+		if (a < b) {
+			minAB = a;
+		}
+		else {
+			minAB = b;
+		}
+		
+		if (c < d) {
+			minCD = c;
+		}
+		else {
+			minCD = d;
+		}
+		
+		System.out.print("The result of expression \'max{min(a, b), min(c, d)\' is " + (minAB > minCD ? minAB : minCD));
+	}
+}
