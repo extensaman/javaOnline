795d1f8749093c13988a30bf1b936ddcf8c3b700
extensaman <49030354+extensaman@users.noreply.github.com>
2021-10-04 12:21:48 +0300

    Раздел "Циклы". Задача №1.
    Напишите программу, где пользователь вводит любое целое положительное число. А программа суммирует все числа от 1 до введенного пользователем числа.

---
+++ 1 Basics/Cycles01.java
@@ -0,0 +1,34 @@
+﻿package by.home.basics;
+
+import java.util.Scanner;
+
+/*
+ * Раздел "Циклы".
+ * Задача №1.
+ * Напишите программу, где пользователь вводит любое целое положительное число. А программа суммирует все числа от 1 до введенного пользователем числа.
+ */
+
+public class Cycles01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Enter a positive integer: ");
+		
+		int x = 0;
+		long sum = 0;
+		
+		if (sc.hasNextInt() && (x = sc.nextInt()) > 0) {
+			for (int i = 1; i <= x; i++) {
+				sum += i;
+			}
+			System.out.println("Sum of integers from 1 to " + x + " is " + sum);
+		}
+		else {
+			System.out.println("Program stopped because you had entered the wrong number.");
+		}
+		sc.close();
+	}
+
+}
