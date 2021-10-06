34a2fed7557b701b6ed3a772ecbf6fdf999a7070
extensaman <49030354+extensaman@users.noreply.github.com>
2021-10-06 13:42:25 +0300

    Раздел "Циклы". Задача №6.
    Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера.

---
+++ 1 Basics/Cycles06.java
@@ -0,0 +1,17 @@
+﻿package by.home.basics;
+
+/*
+ * Раздел "Циклы".
+ * Задача №6.
+ * Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера.
+ */
+
+public class Cycles06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (char i = '0'; i <= 'z'; i++) {
+			System.out.println("Symbol \'" + i + "\' has code " + (byte) i);
+		}
+	}
+}
