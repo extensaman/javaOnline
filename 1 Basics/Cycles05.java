2884677e4eb580e77c581eada16cddec72e202d6
extensaman <49030354+extensaman@users.noreply.github.com>
2021-10-06 13:03:25 +0300

    Раздел "Циклы". Задача №5.
    Даны числовой ряд и некоторое число е. Найти сумму тех членов ряда, модуль которых больше или равен
    заданному е. Общий член ряда имеет вид: An = 1 / pow (2,n) + 1 / pow (3,n)

---
+++ 1 Basics/Cycles05.java
@@ -0,0 +1,45 @@
+﻿package by.home.basics;
+
+import java.util.Scanner;
+
+/*
+ * Раздел "Циклы".
+ * Задача №5.
+ * Даны числовой ряд и некоторое число е. Найти сумму тех членов ряда, модуль которых больше или равен
+ * заданному е. Общий член ряда имеет вид: An = 1 / pow (2,n) + 1 / pow (3,n)
+ */
+
+public class Cycles05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Enter a real number \'e\': ");
+		Scanner sc = new Scanner(System.in);
+
+		while (!sc.hasNextDouble()) {
+			sc.nextLine();
+			System.out.println("Try again. Enter a real number \'e\': ");
+		}
+
+		double e = sc.nextDouble();
+
+		double an = 0.0;
+		double sum = 0.0;
+		int n = 1;
+
+		while (true) {
+			an = 1.0 / Math.pow(2.0, n) + 1.0 / Math.pow(3.0, n);
+			if (Math.abs(an) >= e) {
+				sum += an;
+				n++;
+			} else {
+				break;
+			}
+		}
+		System.out.println("The result of calculation is " + sum);
+		
+		sc.close();
+	}
+		
+}
