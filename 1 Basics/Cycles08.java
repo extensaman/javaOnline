7e9f294c6d848c2fff557dbd5716f14fbbe00d18
extensaman <49030354+extensaman@users.noreply.github.com>
2021-10-08 14:51:22 +0300

    Раздел "Циклы". Задача №8.
    Даны два числа. Определить цифры, входящие в запись как первого так и второго числа.

---
+++ 1 Basics/Cycles08.java
@@ -0,0 +1,51 @@
+﻿package by.home.basics;
+
+/*
+ * Раздел "Циклы".
+ * Задача №8.
+ * Даны два числа. Определить цифры, входящие в запись как первого так и второго числа.
+ */
+
+public class Cycles08 {
+
+	public static void main(String[] args) {
+
+		int a = 201920000;
+		int b = 98932840;
+
+		if (a == 0 && b == 0) {
+			System.out.println("Equal digit in numbers \'a\'=" + a + " \'b\'=" + b + " is 0");
+			return;
+		}
+
+		boolean[] mas = { false, false, false, false, false, false, false, false, false, false };
+		int tempA = a;
+
+		while (tempA != 0) {
+
+			int lastDigitInA = tempA % 10;
+			int tempB = b;
+
+			while (tempB != 0) {
+
+				int lastDigitInB = tempB % 10;
+				if (lastDigitInA == lastDigitInB) {
+					mas[lastDigitInA] = true;
+					break;
+				}
+
+				tempB /= 10;
+			}
+
+			tempA /= 10;
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			
+			if (mas[i]) {
+				System.out.println("Equal digit in numbers \'a\'=" + a + " \'b\'=" + b + " is " + i);
+			}
+		}
+	}
+
+}
