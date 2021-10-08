40c5fd76b1934ebe2aa669d3f7bf0fd49e8a07aa
extensaman <49030354+extensaman@users.noreply.github.com>
2021-10-08 16:34:40 +0300

    Раздел "Одномерные масивы". Задача №1.
    В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.

---
+++ 2 Algorithmization/OneDimensionalArrays01.java
@@ -0,0 +1,35 @@
+﻿package by.home.algorithmization;
+
+/*
+* Этап "Алгоритмизация"
+* Раздел "Одномерные масивы".
+* Задача №1.
+* В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
+*/
+
+public class OneDimensionalArrays01 {
+
+	public static void main(String[] args) {
+		int k = 11;
+		int n = 10;
+		int[] a = new int[n];
+		
+		// Filling an array
+		for (int i = 0; i < n; i++) {
+			a[i] = (int)(Math.random() * 100) + 1;
+			System.out.println("a[" + i + "]=" + a[i]);
+		}
+
+		long sum = 0;
+
+		//Calculation
+		for (int i = 0; i < n; i++) {
+			if (a[i] % k == 0) {
+				sum += a[i];
+			}
+		}
+		
+		System.out.println("The result of calculation is " + sum);
+	}
+
+}
