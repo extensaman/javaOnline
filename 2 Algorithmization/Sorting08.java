dfbd67f02ab954694623d71791f5b115eb7f2157
extensaman <49030354+extensaman@users.noreply.github.com>
2021-10-21 22:27:28 +0300

    Раздел "Сортировка". Задача №8.
    Даны дроби p1/q1, p2/q2, ... , pn/qn (pi, qi - натуральные). 
    Составить программу, которая приводит эти дроби к общему знаменателю и 
    упорядочивает их в порядке возрастания.

--- 2 Algorithmization/Sorting08.java
+++ 2 Algorithmization/Sorting08.java
@@ -16,35 +16,37 @@ public class Sorting08 {
 	public static void main(String[] args) {
 		
 		int n; // dimension of arrays 'p' and 'q'
-		n = 2; //(int) (Math.random() * 3.0) + 2;
+		n = (int) (Math.random() * 4.0) + 2;
 		
 		int[] p = new int[n];
 		int[] q = new int[n];
 		
 		// Initialization of arrays
 		for (int i = 0; i < n; i++) {
-			p[i] = (int) (Math.random() * 9.0) + 1;
-			q[i] = (int) (Math.random() * 9.0) + 1;
+			p[i] = (int) (Math.random() * 8.0) + 1;
+			q[i] = (int) (Math.random() * 8.0) + 1;
 		}
 		
 		// Print generated fractions
 		System.out.println("Original fractions are");
 		printFraction(p, q);
 		
-		// Separate each 'q'-element to prime multipliers
+		// Separate each denominator ('q'-element) to prime multipliers
 		
 		int[][] arrayPrimeMultiplier = new int[n][];
 		
-		System.out.println();
+		System.out.print("\n\nSeparate each denominator ('q'-element) to prime multipliers:");
 		for (int i = 0; i < n; i++) {
 			arrayPrimeMultiplier[i] = separateToPrimeMultiplier(q[i]);
-			System.out.println();
-			printArray(arrayPrimeMultiplier[i]);
 		}
 		
-		int[] nod = new int[n];
-		arrayInitialization(nod, 1);
-		
+		System.out.println("\n\nCalculations...");
+		// Calculate general greatest common factor (GGCF) **Общий НОД** of all denominators
+		// result will be in nod[n - 1]
+		int nod = 0;
+		int nok = 1;
+		int previousNok = q[0];
+				
 		for (int i = 0; i < n - 1; i++) {
 			
 			for (int j = 0; arrayPrimeMultiplier[i][j] != 0; j++) {
@@ -66,12 +68,24 @@ public class Sorting08 {
 				}
 			}
 			
-			nod[i] = calcNod(arrayPrimeMultiplier[i]);
-			System.out.println("\nNOD of " + q[i] + " and " + q[i+1] + " is " + nod[i]);
-			arrayPrimeMultiplier[i+1] = separateToPrimeMultiplier(nod[i]);
+			nod = calcNod(arrayPrimeMultiplier[i]);
+			nok = previousNok * q[i + 1] / nod;
+			System.out.print("\nnod is " + nod + " nok is " + nok + ")");
+			arrayPrimeMultiplier[i+1] = separateToPrimeMultiplier(nok);
+			previousNok = nok;
+		}
+
+		System.out.println("\nLeast common multiple of all denominators is " + nok);
+		
+		// Reduce fractions to a common denominator
+		for (int i = 0; i < n; i++) {
+			p[i] *= nok / q[i];
+			q[i] = (int) nok;
 		}
 		
-		nod[n-1] = calcNod(arrayPrimeMultiplier[n-1]);
+		// Print generated fractions
+		System.out.println("\nFractions with common denominator are");
+		printFraction(p, q);
 	}
 
 	public static int[] separateToPrimeMultiplier(int q) {
@@ -79,6 +93,9 @@ public class Sorting08 {
 		int[] elementPrimeMultiplier = new int[MAX_COUNT_OF_PRIME];
 		
 		int index = 0;
+		
+		System.out.print("\nPrime-multipliers of " + q + " are");
+		
 		do {
 			for (int i = 2; i <= q; i++) {
 				if (q % i == 0) {
@@ -96,6 +113,9 @@ public class Sorting08 {
 		}
 		
 		elementPrimeMultiplier[index] = 1;
+		
+		printArray(elementPrimeMultiplier);
+		
 		return elementPrimeMultiplier;
 	}
 	
@@ -138,14 +158,12 @@ public class Sorting08 {
 		}
 	}
 	
-	public static void arrayInitialization (int[] a, int var) {
-		
-		for (int i = 0; i < a.length; i++) {
-			a[i] = var;
-		}
-	}
-	
 	public static int calcNod (int[] prime) {
+		
+		if (prime == null) {
+			System.out.println("Bad input parameter in \'int calcNod(int[] mas)\'.");
+			System.exit(0);
+		}
 		
 		int nod = 1;
 		for (int i = 0; prime[i] != 0; i++) {
