62ceb4f1765ba67b7481dd1dd8afaaef2f0dcb8a
extensaman <49030354+extensaman@users.noreply.github.com>
2021-10-23 15:31:34 +0300

    Раздел "Массивы массивов". Задача №6.
    Сформировать квадратную матрицу порядка n по заданному образцу(n - четное):
     1  1  1 ... 1  1  1
     0  1  1 ... 1  1  0
     0  0  1 ... 1  0  0
     .  .  . ...  . .  .
     0  0  1 ... 1  0  0
     0  1  1 ... 1  1  0
     1  1  1 ... 1  1  1

--- 2 Algorithmization/MultiDimensionalArrays/MultiDimensionalArrays06.java
+++ 2 Algorithmization/MultiDimensionalArrays/MultiDimensionalArrays06.java
@@ -1,4 +1,4 @@
-package by.home.algorithmization;
+package by.home.algorithmization.multidimensionalarrays;
 
 /*
 * Этап "Алгоритмизация"
@@ -20,8 +20,10 @@ public class MultiDimensionalArrays06 {
 
 		// Generate array dimension 
 		int m = 0;
+		
 		do {
 			m = (int) (Math.random() * 10.0) + 2;
+			
 		} while (m % 2 != 0);
 		
 		
@@ -30,17 +32,22 @@ public class MultiDimensionalArrays06 {
 		
 		// Fill upper part of the array
 		for (int i = 0; i < a.length / 2; i++) {
+			
 			for (int j = i; j < a.length - i; j++) {
+				
 				a[i][j] = 1;
 			}
 		}
 		
 		// Fill lower part of the array
 		for (int i = a.length - 1; i >= a.length / 2; i--) {
+			
 			for (int j = i; j >= a.length - i - 1; j--) {
+				
 				a[i][j] = 1;
 			}
 		}
+		
 		// Print result
 		System.out.println("Array " + m + 'x' + m + ":");
 		printArray(a);
@@ -49,14 +56,18 @@ public class MultiDimensionalArrays06 {
 	public static void printArray (int[][] mas) {
 		
 		if (mas == null) {
+			
 			System.out.println("Bad array reference.");
 			return;
 		}
 		
 		for (int i = 0; i < mas.length; i++) {
+			
 			for (int j = 0; j < mas[i].length; j++) {
+				
 				System.out.printf("%4d", mas[i][j]);
 			}
+			
 			System.out.println();
 		}
 	}
