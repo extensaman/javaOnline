a7d929d71a8964a08bebb8d00a53b66ee86dc092
extensaman <49030354+extensaman@users.noreply.github.com>
2021-10-23 15:51:16 +0300

    Раздел "Массивы массивов". Задача №10.
    Найти положительные элементы главной диагонали квадратной матрицы.

--- 2 Algorithmization/MultiDimensionalArrays/MultiDimensionalArrays10.java
+++ 2 Algorithmization/MultiDimensionalArrays/MultiDimensionalArrays10.java
@@ -1,4 +1,4 @@
-package by.home.algorithmization;
+package by.home.algorithmization.multidimensionalarray;
 
 /*
 * Этап "Алгоритмизация"
@@ -13,8 +13,10 @@ public class MultiDimensionalArrays10 {
 		
 		// Generate array dimensions 
 		int m = 0;
+		
 		do {
 			m = (int) (Math.random() * 10.0) + 2;
+			
 		} while (m % 2 != 0);
 		
 		//Create an array
@@ -22,19 +24,24 @@ public class MultiDimensionalArrays10 {
 
 		// Generate array elements
 		for (int i = 0; i < m; i++) {
+			
 			for (int j = 0; j < m; j++) {
+				
 				a[i][j] = (int) (Math.random() * 10.0) - 5;
 			}
 		}
 
 		// Print array
 		System.out.println("Array " + m + 'x' + m + " is\n");	
+		
 		printArray(a);
 		
 		// Print positive elements of the main diagonal
 		System.out.println("\nPositive elements of the main diagonal are");
 		for (int i = 0; i < m; i++) {
+			
 			if (a[i][i] > 0) {
+				
 				System.out.printf("%4d", a[i][i]);
 			}
 		}
@@ -43,15 +50,19 @@ public class MultiDimensionalArrays10 {
 	
 	public static void printArray(int[][] mas) {
 
-		if (mas == null) {
+		if (mas == null) { 
+			
 			System.out.println("Bad array reference.");
 			return;
 		}
 
 		for (int i = 0; i < mas.length; i++) {
+			
 			for (int j = 0; j < mas[i].length; j++) {
+				
 				System.out.printf("%4d", mas[i][j]);
 			}
+			
 			System.out.println();
 		}
 	}
