857ef3604cf7b102bfd2b08b845fe07cc70a1c2d
extensaman <49030354+extensaman@users.noreply.github.com>
2021-10-23 15:30:06 +0300

    Раздел "Массивы массивов". Задача №5.
    Сформировать квадратную матрицу порядка n по заданному образцу(n - четное):
     1  1  1 ... 1  1  1
     2  2  2 ... 2  2  0
     3  3  3 ... 3  0  0
     .  .  . ...  . .  .
     n-1 n-1 0 ... 0  0  0
     n  0  0 ... 0  0  0

--- 2 Algorithmization/MultiDimensionalArrays/MultiDimensionalArrays05.java
+++ 2 Algorithmization/MultiDimensionalArrays/MultiDimensionalArrays05.java
@@ -1,4 +1,4 @@
-package by.home.algorithmization;
+package by.home.algorithmization.multidimensionalarrays;
 
 /*
 * Этап "Алгоритмизация"
@@ -19,8 +19,10 @@ public class MultiDimensionalArrays05 {
 		
 		// Generate array dimension 
 		int m = 0;
+		
 		do {
 			m = (int) (Math.random() * 8.0) + 2;
+			
 		} while (m % 2 != 0);
 		
 		
@@ -29,7 +31,9 @@ public class MultiDimensionalArrays05 {
 		
 		// Fill the array
 		for (int i = 0; i < a.length; i++) {
+			
 			for (int j = 0; j < a.length - i; j++) {
+				
 				a[i][j] = i + 1;
 			}
 		}
@@ -42,14 +46,18 @@ public class MultiDimensionalArrays05 {
 	public static void printArray (int[][] mas) {
 		
 		if (mas == null) {
+			
 			System.out.println("Bad array reference.");
 			return;
 		}
 		
 		for (int i = 0; i < mas.length; i++) {
+			
 			for (int j = 0; j < mas[i].length; j++) {
+				
 				System.out.printf("%4d", mas[i][j]);
 			}
+			
 			System.out.println();
 		}
 	}
