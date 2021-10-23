a621351f9085d69d8fb0a2bf0282afb947006aa0
extensaman <49030354+extensaman@users.noreply.github.com>
2021-10-23 15:22:02 +0300

    Раздел "Массивы массивов". Задача №1.
    Дана матрица. Вывести на экран все нечетные столбцы, у которых первый элемент больше последнего

--- 2 Algorithmization/MultiDimensionalArrays/MultiDimensionalArrays01.java
+++ 2 Algorithmization/MultiDimensionalArrays/MultiDimensionalArrays01.java
@@ -1,4 +1,4 @@
-package by.home.algorithmization;
+package by.home.algorithmization.multidimensionalarrays;
 
 /*
 * Этап "Алгоритмизация"
@@ -22,7 +22,9 @@ public class MultiDimensionalArrays01 {
 		
 		// Generate array elements
 		for (int i = 0; i < m; i++) {
+			
 			for (int j = 0; j < n; j++) {
+				
 				a[i][j] = (int) (Math.random() * 10.0) - 5;
 			}
 		}
@@ -32,7 +34,9 @@ public class MultiDimensionalArrays01 {
 		// Search and print odd columns where first element more than the last one
 		System.out.println("\nResult:");
 		for (int i = 1; i < a[0].length; i += 2) {
+			
 			if (a[0][i] > a[a.length - 1][i]) {
+				
 				printArrayColumn(a, i);
 			}
 		}
@@ -41,14 +45,18 @@ public class MultiDimensionalArrays01 {
 	public static void printArray (int[][] mas) {
 		
 		if (mas == null) {
+			
 			System.out.println("Bad array reference.");
 			return;
 		}
 		
 		for (int i = 0; i < mas.length; i++) {
+			
 			for (int j = 0; j < mas[i].length; j++) {
+				
 				System.out.printf("%4d", mas[i][j]);
 			}
+			
 			System.out.println();
 		}
 	}
@@ -56,14 +64,17 @@ public class MultiDimensionalArrays01 {
 	public static void printArrayColumn (int[][] mas, int columnIndex) {
 		
 		if (mas == null) {
+			
 			System.out.println("Bad array reference.");
 			return;
 		}
 		
 		System.out.println("Column with index " + columnIndex + ":");
 		for (int i = 0; i < mas.length; i++) {
+			
 			System.out.printf("%4d", mas[i][columnIndex]);
 		}
+		
 		System.out.println();
 	}
 }
