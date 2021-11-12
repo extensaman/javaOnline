9ba8b035ac2fda777921562a084f55b356cc3b56
extensaman <49030354+extensaman@users.noreply.github.com>
2021-11-12 11:36:39 +0300

    Раздел "Простейшие классы и объекты". Задача №03.
    Класс Runner предназначен для обеспечения возможности функционирования классов Student и StudentSample

--- 4 Programming with classes/The simplest classes and objects/Simple03/Runner.java
+++ 4 Programming with classes/The simplest classes and objects/Simple03/Runner.java
@@ -1,16 +1,29 @@
 package by.home.programmingclass.simple03;
 
+/**
+* Этап "Programming with classes"
+* Раздел "Простейшие классы и объекты".
+* Задача №03.
+* 
+* Класс Runner предназначен для обеспечения возможности функционирования классов Student и StudentSample
+* 
+* @author extensaman
+* @version 1.0
+*/
+
 public class Runner {
 
 	public static void main(String[] args) {
 		
-		Student st1 = new Student ();
 		System.out.println ("-= Empty student creation example =-\n");
+		Student st1 = new Student ();
 		System.out.println(st1);
 		
+		System.out.println ("-= Student creation example =-\n");
 		Student st2 = new Student ("Иванов И.И.", "А1290", new byte[] {2,3,7,8,9});
 		System.out.println(st2);
 		
+		System.out.println ("-= Creation and adding students to just created student's sample =-\n");
 		StudentSample studentSample = new StudentSample ();
 		
 		studentSample.addStudentToSample(st1);
@@ -26,6 +39,7 @@ public class Runner {
 		
 		System.out.println(studentSample);
 		
+		System.out.println ("-= Create a sample of students with marks higher than 9 =-\n");
 		StudentSample bestStudentSample = studentSample.sampleOfBestStudent(9);
 		
 		System.out.println(bestStudentSample);
